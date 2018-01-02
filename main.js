cda60f17f255f68682408c17e9098455e8b9ada1
ro-rnai <n1244506804@yahoo.com.tw>
2018-01-02 12:50:23 +0800

    coockie保留上次設定及過期顯示

--- main.js
+++ main.js
@@ -10,11 +10,23 @@ function run()
 		str=str[0].concat(str[1]);
 		for(i=0;i<str.length;++i)
 			str[i]=parseInt(str[i]);
-		console.log(str);
 		var d=new Date(str[0],str[1]-1,str[2],str[3],str[4],0,0);
 		return d.getTime();
 	}
-	refreshData(getTimeStamp(st.value),getTimeStamp(ft.value),getTimeStamp(tt.value));
+	function setCookie(t1,t2,t3)
+	{
+		var d=new Date();
+		d.setTime(t3);
+		document.cookie="setTime="+t1+";expires="+d.toGMTString();
+		document.cookie="fromTime="+t2+";expires="+d.toGMTString();
+		document.cookie="toTime="+t3+";expires="+d.toGMTString();
+	}
+	var set_t,form_t,to_t;
+	set_t=getTimeStamp(st.value);
+	form_t=getTimeStamp(ft.value);
+	to_t=getTimeStamp(tt.value);
+	refreshData(set_t,form_t,to_t);
+	setCookie(set_t,form_t,to_t);
 }
 function show(idx)
 {
@@ -75,18 +87,35 @@ function refreshData(set_t,from_t,to_t)
 		{
 			d.setTime(A[i][0]*60000);
 			TB[0].push();
-			TB[0].push("星期"+d_arr[d.getDay()]);
+			TB[0].push({str:"星期"+d_arr[d.getDay()]});
 			TB[1].push();
-			TB[1].push((d.getMonth()+1)+"月"+d.getDate()+"日");
+			TB[1].push({str:(d.getMonth()+1)+"月"+d.getDate()+"日"});
 		}
 		for(i=0;i<nrow;++i)
 		{
-			TB.push([(0<=i-off && i-off<A[0].length?getHM(A[0][i-off]):""),
-			         (0<=i-off && i-off<A[0].length?getHM(A[0][i-off]+10):"")]);
+			TB.push([]);
+			if(0<=i-off && i-off<A[0].length)
+			{
+				TB[i+2].push({str:getHM(A[0][i-off]),ts:A[0][i-off]});
+				TB[i+2].push({str:getHM(A[0][i-off]+10),ts:A[0][i-off]+10});
+			}
+			else
+			{
+				TB[i+2].push({str:"",ts:null});
+				TB[i+2].push({str:"",ts:null});
+			}
 			for(j=1;j<ncol;++j)
 			{
-				TB[i+2].push(i<A[j].length?getHM(A[j][i]):"");
-				TB[i+2].push(i<A[j].length?getHM(A[j][i]+10):"");
+				if(i<A[j].length)
+				{
+					TB[i+2].push({str:getHM(A[j][i]),ts:A[j][i]});
+					TB[i+2].push({str:getHM(A[j][i]+10),ts:A[j][i]+10});
+				}
+				else
+				{
+					TB[i+2].push({str:"",ts:null});
+					TB[i+2].push({str:"",ts:null});
+				}
 			}
 		}
 		return TB;
@@ -97,7 +126,7 @@ function refreshData(set_t,from_t,to_t)
 		}
 	}
 	//將表格輸出到 preview
-	var i,j,tb,tr,td;
+	var i,j,tb,tr,td,cur_ts;
 	preview.appendChild(tb=document.createElement("table"));
 	for(i=0;i<2;++i)
 	{
@@ -110,19 +139,22 @@ function refreshData(set_t,from_t,to_t)
 				td.setAttribute("style","color:#0000DD");
 			else
 				td.setAttribute("style","color:#339966");
-			td.appendChild(document.createTextNode(TB[i][j]));
+			td.appendChild(document.createTextNode(TB[i][j].str));
 		}
 	}
 	d.setTime(A[0][0]*60000);
 	t=d.getDay();
+	cur_ts=(Date.now()/60000|0);
 	for(;i<TB.length;++i)
 	{
 		tb.appendChild(tr=document.createElement("tr"));
 		for(j=0;j<TB[i].length;++j)
 		{
 			tr.appendChild(td=document.createElement("td"));
-			td.appendChild(document.createTextNode(TB[i][j]));
-			td.setAttribute("style","background-color:"+bg_color[(t+(j/2|0))%7]);
+			td.appendChild(document.createTextNode(TB[i][j].str));
+			td.setAttribute("style","background-color:"+bg_color[(t+(j/2|0))%7]+
+			(TB[i][(j/2|0)*2].ts+10<cur_ts?";color:rgba(0,0,0,0.25)":"")
+			);
 		}
 	}
 	preview.appendChild(tb=document.createElement("p"));
@@ -131,11 +163,11 @@ function refreshData(set_t,from_t,to_t)
 	var str="[table cellspacing=0 cellpadding=3 border=1]";
 	str+="[tr]";
 	for(j=0;j<TB[0].length;++j)
-		str+="[td colspan=2 align=center][b][color=#0000DD]"+TB[0][j]+"[/color][/b][/td]";
+		str+="[td colspan=2 align=center][b][color=#0000DD]"+TB[0][j].str+"[/color][/b][/td]";
 	str+="[/tr]";
 	str+="[tr]";
 	for(j=0;j<TB[0].length;++j)
-		str+="[td colspan=2 align=center][b][color=#339966]"+TB[1][j]+"[/color][/b][/td]";
+		str+="[td colspan=2 align=center][b][color=#339966]"+TB[1][j].str+"[/color][/b][/td]";
 	str+="[/tr]";
 	d.setTime(A[0][0]*60000);
 	t=d.getDay();
@@ -144,7 +176,7 @@ function refreshData(set_t,from_t,to_t)
 		str+="[tr]";
 		for(j=0;j<TB[i].length;++j)
 		{
-			str+="[td align=center bgcolor="+bg_color[(t+(j/2|0))%7]+"]"+TB[i][j]+"[/td]";
+			str+="[td align=center bgcolor="+bg_color[(t+(j/2|0))%7]+"]"+TB[i][j].str+"[/td]";
 			//td.appendChild(document.createTextNode(TB[i][j]));
 			//td.setAttribute("style","background-color:"+bg_color[(t+(j/2|0))%7]);
 		}
@@ -187,15 +219,15 @@ function refreshData(set_t,from_t,to_t)
 		ctx.font="bold 16px Arial,PMingLiU";
 		ctx.fillStyle="#0000DD";
 		for(j=0;j<TB[0].length;++j)
-			ctx.fillText(TB[0][j],j*2*53+54,0*27+14);
+			ctx.fillText(TB[0][j].str,j*2*53+54,0*27+14);
 		ctx.fillStyle="#339966";
 		for(j=0;j<TB[1].length;++j)
-			ctx.fillText(TB[1][j],j*2*53+54,1*27+14);
+			ctx.fillText(TB[1][j].str,j*2*53+54,1*27+14);
 		ctx.fillStyle="#000000";
 		ctx.font="16px Arial,PMingLiU";
 		for(i=2;i<TB.length;++i)
 			for(j=0;j<TB[i].length;++j)
-				ctx.fillText(TB[i][j],j*53+27,i*27+14);
+				ctx.fillText(TB[i][j].str,j*53+27,i*27+14);
 		var p;
 		div.appendChild(p=document.createElement("p"));
 		p.appendChild(document.createTextNode("可點擊圖片下載"))
@@ -227,7 +259,7 @@ function init()
 	tt=document.getElementById("tt");
 	preview=document.getElementById("preview");
 	baha=document.getElementById("baha");
-	
+	//設定本週時間
 	var d=new Date();
 	st.value=getTimeStr(d);
 	var t=(d.getDay()+5)%7;
@@ -245,15 +277,25 @@ function init()
 		return [dt.getFullYear(),dt.getMonth()+1,dt.getDate()].join("/")
 		       +" "+("0"+dt.getHours()).substr(-2)+":"+("0"+dt.getMinutes()).substr(-2);
 	}
-	
+	//設定背景色調色盤
 	var i;
 	bg_color=[];
 	for(i=0;i<7;++i)
 	{
 		bg_color.push(HSV2CODE(60+360/7*(i*2+1),0.3,1));
 	}
-	
-	
+	//檢查coockie，若有則覆寫本週時間
+	var ck=getCookie();
+	if(ck["setTime"] && ck["fromTime"] && ck["toTime"])
+	{
+		d.setTime(ck["setTime"]);
+		st.value=getTimeStr(d);
+		d.setTime(ck["fromTime"]);
+		ft.value=getTimeStr(d);
+		d.setTime(ck["toTime"]);
+		tt.value=getTimeStr(d);
+		refreshData(ck["setTime"],ck["fromTime"],ck["toTime"]);
+	}
 	function HSV2CODE(h,s,v)
 	{
 		h=h%360;
@@ -285,6 +327,18 @@ function init()
 			return "#"+("0"+red.toString(16)).substr(-2)+("0"+green.toString(16)).substr(-2)+("0"+blue.toString(16)).substr(-2);
 		}
 	}
-	
+	function getCookie()
+	{
+		var ret=[];
+		var r=document.cookie.split(";");
+		var i;
+		for(i=r.length-1;i>=0;--i)
+		{
+			r[i]=r[i].split('=');
+			if(r[i].length==2)
+				ret[r[i][0].trim()]=r[i][1].trim();
+		}
+		return ret;
+	}
 	
 }
